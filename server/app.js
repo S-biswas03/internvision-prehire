aeaf9b1bbe6e1a0eb51320b8feeb98a4abfcfbc7
S-biswas03 <supritam593@gmail.com>
2026-08-04 17:08:20 +0530

    Fix CORS for Vercel

--- server/app.js
+++ server/app.js
@@ -15,7 +15,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://internvision-prehire.vercel.app",
+    ],
     credentials: true,
   }),
 );
